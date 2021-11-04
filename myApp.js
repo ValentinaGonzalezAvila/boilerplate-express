3771f6ec6440d1b7fb57f06e0309bf7482d43168
ValentinaGonzalezAvila <82173439+ValentinaGonzalezAvila@users.noreply.github.com>
2021-11-04 10:21:35 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
-
-
+//Primer tarea
+console.log("Hello Express");
+//Segunda tarea
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+//Tercera tarea
 
 
 
